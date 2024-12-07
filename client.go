34598cc5314d8a98ca0a813e756e0f53615a397d
devcal <dev@devcal.dev>
2024-12-07 15:59:24 +0100

    wip

--- client.go
+++ client.go
@@ -2,10 +2,13 @@ package client
 
 import (
 	"context"
+	"encoding/json"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/devcaldev/devcal-go/rpc"
 )
@@ -25,21 +28,58 @@ func (c *apiKeyCredentials) RequireTransportSecurity() bool {
 	return c.requireTransportSecurity
 }
 
-type grpcClient struct {
-	rpc.EventsServiceClient
-	c *grpc.ClientConn
+type Client struct {
+	events rpc.EventsServiceClient
+	c      *grpc.ClientConn
 }
 
-func (c *grpcClient) Close() error {
+type Event struct {
+	ID        string
+	AccountID string
+	Dtstart   time.Time
+	Dtend     time.Time
+	Rrule     string
+	Props     map[string]any
+}
+
+type InsertEventParams struct {
+	Dtstart time.Time
+	Dtend   time.Time
+	Rrule   string
+	Props   map[string]any
+}
+
+func (c *Client) InsertEvent(p *InsertEventParams) (*Event, error) {
+	rp := &rpc.InsertEventParams{
+		Dtstart: timestamppb.New(p.Dtstart),
+		Dtend:   timestamppb.New(p.Dtend),
+		Rrule:   &p.Rrule,
+	}
+	rp.Props, _ = json.Marshal(p.Props)
+
+	re, err := c.events.InsertEvent(
+		context.Background(),
+		rp,
+	)
+	if err != nil {
+		return nil, err
+	}
+	e := &Event{
+		ID:        re.ID,
+		AccountID: re.AccountID,
+		Dtstart:   re.Dtstart.AsTime(),
+		Dtend:     re.Dtend.AsTime(),
+		Rrule:     re.Rrule,
+	}
+	json.Unmarshal(re.Props, &e.Props)
+	return e, nil
+}
+
+func (c *Client) Close() error {
 	return c.c.Close()
 }
 
-type Client interface {
-	rpc.EventsServiceClient
-	Close() error
-}
-
-func New(addr, apiKey string) (Client, error) {
+func New(addr, apiKey string) (*Client, error) {
 	return NewWithOptions(
 		addr,
 		apiKey,
@@ -48,7 +88,7 @@ func New(addr, apiKey string) (Client, error) {
 	)
 }
 
-func NewWithInsecureCredentials(addr, apiKey string) (Client, error) {
+func NewWithInsecureCredentials(addr, apiKey string) (*Client, error) {
 	return NewWithOptions(
 		addr,
 		apiKey,
@@ -57,15 +97,15 @@ func NewWithInsecureCredentials(addr, apiKey string) (Client, error) {
 	)
 }
 
-func NewWithOptions(addr string, apiKey string, opts ...grpc.DialOption) (Client, error) {
+func NewWithOptions(addr string, apiKey string, opts ...grpc.DialOption) (*Client, error) {
 	conn, err := grpc.NewClient(addr, opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	c := &grpcClient{
-		rpc.NewEventsServiceClient(conn),
-		conn,
+	c := &Client{
+		events: rpc.NewEventsServiceClient(conn),
+		c:      conn,
 	}
 
 	return c, nil
